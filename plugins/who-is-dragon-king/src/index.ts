7de80a8d9cb913e09ec1a030e6005d8e9a349251
blanquizel <blanquizel@gmail.com>
2022-07-13 02:26:41 +0800

    use star emo instead word

--- plugins/who-is-dragon-king/src/index.ts
+++ plugins/who-is-dragon-king/src/index.ts
@@ -1,7 +1,14 @@
-import { Context } from 'koishi'
+import { Context, Logger } from 'koishi'
 
 export const name = 'who-is-dragon-king'
 
+const record : Map<string, number> = new Map();
+
+const core = (ctx: Context) => {
+    const log = ctx.logger('who-is-dragon-king');
+    log.debug('who-is-dragon-king test');
+}
+
 export function apply(ctx: Context) {
-  // write your plugin here
+    ctx.plugin(core);
 }
