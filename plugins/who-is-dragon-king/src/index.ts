10880c3742dc198dd0d97131bb09078ab6edc1a3
blanquizel <blanquizel@gmail.com>
2022-07-13 04:02:04 +0800

    add dialog count plugin

--- plugins/who-is-dragon-king/src/index.ts
+++ plugins/who-is-dragon-king/src/index.ts
@@ -2,11 +2,69 @@ import { Context, Logger } from 'koishi'
 
 export const name = 'who-is-dragon-king'
 
-const record : Map<string, number> = new Map();
+const data = new Map();
 
 const core = (ctx: Context) => {
     const log = ctx.logger('who-is-dragon-king');
     log.debug('who-is-dragon-king test');
+
+    ctx.command('今日龙王')
+        .userFields(['name'])
+        .action(({ session }) => {
+            const guildId = session.guildId;
+            if (!data.has(guildId)) {
+                return '暂无人发言';
+            }
+
+            const { record, userMap } = data.get(guildId);
+            const list = [];
+            let count = 0;
+            for (let item of record.entries()) {
+                console.log(item);
+                list.push(item);
+                count += item[1];
+            }
+            list.sort((a, b) => {
+                return a[1] - b[1];
+            });
+
+
+            let str = '';
+            str += `今日龙王榜(共${count}条消息)\n`;
+
+            for (let i = 0; i < Math.min(list.length, 5); i++) {
+                str += `${i+1} - ${userMap.get(list[i][0])} 共${list[i][1]}条\n`;
+            }
+
+            return str;
+        })
+
+    ctx.middleware(async (session, next) => {
+        console.log(session);
+        const guildId = session.guildId;
+        const userId = session.author.userId;
+        const username = session.author.nickname === '' ? session.author.username : session.author.nickname;
+
+        if (data.has(guildId)) {
+            const { record, userMap } = data.get(guildId);
+            if (!record.has(userId)) {
+                record.set(userId, record.get(userId) + 1);
+            } else {
+                record.set(userId, 1);
+            }
+            if (!userMap.has(userId)) {
+                userMap.set(userId, username);
+            }
+        } else {
+            const record = new Map();
+            const userMap = new Map();
+            record.set(userId, 1);
+            userMap.set(userId, username);
+            data.set(guildId, { record, userMap });
+        }
+
+        return next();
+    })
 }
 
 export function apply(ctx: Context) {
